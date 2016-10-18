9fc1c868cd57424800de27a8955f32b2d6a4df66
殷华中 <491684254@qq.com>
2016-10-18 23:20:33 +0800

    D3 Training

---
+++ D3/D3/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace D3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SelectorDemo()
+        {
+            return View();
+        }
+
+        public ActionResult EnterUpdateExitDemo()
+        {
+            return View();
+        }
+
+        public ActionResult D3ArrayMethod()
+        {
+            return View();
+        }
+    }
+}
---
+++ D3/D3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="D3.Global" Language="C#" %>
